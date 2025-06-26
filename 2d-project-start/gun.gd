bd6cad1f0b77a59b52859b8a067a96b327d2c803
James Christensen <jchristensen0713@gmail.com>
2025-06-26 17:50:15 -0600

    Clean up

--- 2d-project-start/gun.gd
+++ 2d-project-start/gun.gd
@@ -3,5 +3,7 @@ extends Area2D
 func _physics_process(delta):
 	var enemies_in_range = get_overlapping_areas()
 	if enemies_in_range.size() > 0:
+		print("Enemies in Range")
 		var target_enemy = enemies_in_range.front()
+		print("target_enemy found")
 		look_at(target_enemy.global_position)
