a9ee082024be2d384410ce9bc4fe5ddd3f87acf7
James Christensen <jchristensen0713@gmail.com>
2025-06-26 22:02:26 -0600

    hp, damage, player hp, path, path2d follow

--- 2d-project-start/player.gd
+++ 2d-project-start/player.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
-
+signal health_depleted
 @onready var happy = $HappyBoo
 
+var health = 100.0
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
@@ -12,3 +13,11 @@ func _physics_process(delta):
 		happy.play_walk_animation()
 	else: 
 		happy.play_idle_animation()
+		
+	const DAMAGE_RATE = 5.0
+	var overlapping_mobs = 	%HurtBox.get_overlapping_bodies()
+	if overlapping_mobs.size() > 0:
+		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
+		%ProgressBar.value = health
+		if health  <= 0:
+			health_depleted.emit()
