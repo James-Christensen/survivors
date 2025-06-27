f3da39ce2bed1b6ef5f946db30e275c70c21d232
James Christensen <jchristensen0713@gmail.com>
2025-06-26 20:28:23 -0600

    debugging collission issue

--- 2d-project-start/gun.gd
+++ 2d-project-start/gun.gd
@@ -1,9 +1,21 @@
 extends Area2D
 
 func _physics_process(delta):
+	var all_areas = get_overlapping_areas()
+	print("Total overlapping areas: ", all_areas.size())
+	for area in all_areas:
+		print("Found area: ", area.name)
+	
 	var enemies_in_range = get_overlapping_areas()
 	if enemies_in_range.size() > 0:
 		print("Enemies in Range")
 		var target_enemy = enemies_in_range.front()
 		print("target_enemy found")
 		look_at(target_enemy.global_position)
+
+
+func _ready():
+	# Enable collision shape visibility
+	for child in get_children():
+		if child is CollisionShape2D:
+			child.debug_color = Color.RED
