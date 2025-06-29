5f9d2fd70afc96a58ac6a5847539a2b0b666f203
James-Christensen <jchristensen0713@gmail.com>
2025-06-29 14:57:47 -0600

    Bolt animation and blue orb

---
+++ 2d-project-start/Scenes/player/orb.gd
@@ -0,0 +1,42 @@
+extends Area2D
+
+var target_enemy = null
+
+@onready var animation_player = %AnimationPlayer
+
+func _ready():
+	animation_player.play("default")
+
+func _process(delta):
+	var enemies_in_range = get_overlapping_bodies()
+	
+	if enemies_in_range.size() > 0:
+		target_enemy = get_closest_enemy(enemies_in_range)
+		look_at(target_enemy.global_position)
+	else:
+		target_enemy = null
+
+func get_closest_enemy(enemies: Array) -> Node2D:
+	var closest_enemy = null
+	var closest_distance = INF
+	
+	for enemy in enemies:
+		var distance = global_position.distance_to(enemy.global_position)
+		if distance < closest_distance:
+			closest_distance = distance
+			closest_enemy = enemy
+	
+	return closest_enemy
+
+func shoot():
+	# Only shoot if we have a target
+	if target_enemy == null:
+		return
+		
+	const BULLET = preload("res://Scenes/player/bolt.tscn")
+	var new_bullet = BULLET.instantiate()
+	new_bullet.global_transform = %ShootingPoint.global_transform
+	%ShootingPoint.add_child(new_bullet)
+
+func _on_timer_timeout() -> void:
+	shoot()
